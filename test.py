fb5faef545133cfbabfe375fee94f19fb07119ac
Ahmed Abouslima <s17c78@umwelt-campus.de>
2021-07-19 01:30:35 +0200

    Finalizing scripts

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import requests
 
-json_data = {"github": "https://github.com/DigitalProductschool/website", "email": "name@example.com",
-             "url": "https://digitalproductschool.io/", "notes": "I deployed using ..."}
+headers = {"Content-Type": "application/json"}
 
-headers = {"Content-Type": "application/json", "Authorization": "Basic Key"}
-
-# https://dps-challenge.netlify.app/.netlify/functions/api/challenge
 response = requests.post("http://18.117.128.71:5000/api/predictions", headers=headers,
-                         json=json_data)
+                         json={"year": "2021", "month": "6"})
 
 print(response.text)
